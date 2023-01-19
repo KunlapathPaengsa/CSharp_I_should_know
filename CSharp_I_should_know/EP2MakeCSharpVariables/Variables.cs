1c7851ca2960ed0769e0de5c0e475136a98b6a49
Kunlapath <kunlapath.paengsa@gmail.com>
2023-01-19 16:27:37 +0700

    Create EP2 Variables

--- CSharp_I_should_know/EP2MakeCSharpVariables/Variables.cs
+++ CSharp_I_should_know/EP2MakeCSharpVariables/Variables.cs
@@ -2,5 +2,37 @@
 {
     public class Variables
     {
+        int myNum = 5;
+        double myDoubleNum = 5.99D;
+        char myLetter = 'D';
+        bool myBool = true;
+        string myText = "Hello";
+
+        void TypeCasting()
+        {
+            int myInt = 9;
+            double myDouble = myInt;
+
+            double myNewDouble = 9.78;
+            int myNewInt = (int)myNewDouble;
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myNewDouble);
+            Console.WriteLine(myNewInt);
+        }
+
+        void TypeConversion()
+        {
+            Console.WriteLine(Convert.ToString(myNum));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myNum));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDoubleNum));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+        }
+
+        public void GoToEP2()
+        {
+            TypeCasting();
+            TypeConversion();
+        }
     }
 }
--- CSharp_I_should_know/Program.cs
+++ CSharp_I_should_know/Program.cs
@@ -1,4 +1,6 @@
 ﻿using CSharp_I_should_know.EP1WhyIChooseCSharp;
+using CSharp_I_should_know.EP2MakeCSharpVariables;
 
 Console.WriteLine("Hello guy. This is a new series I've started regarding C# tips I should remember.");
 new Introduce().GoToEP1();
+new Variables().GoToEP2();
