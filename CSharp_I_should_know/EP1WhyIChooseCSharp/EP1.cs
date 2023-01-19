5d3d92a76e02d45d62daa07576c4dfe93a4b2523
Kunlapath <kunlapath.paengsa@gmail.com>
2023-01-19 15:52:01 +0700

    Write EP1

---
+++ CSharp_I_should_know/EP1WhyIChooseCSharp/EP1.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace CSharp_I_should_know.EP1WhyIChooseCSharp
+{
+    public interface IEP1
+    {
+        void GoToEP1();
+    }
+
+    public class EP1 : IEP1
+    {
+        public void GoToEP1()
+        {
+            var process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            process.StandardInput.WriteLine("start chrome https://medium.com/@kunlapath.paengsa/c-i-should-know-ep-1-why-i-choose-c-c82def0aebea");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.StandardOutput.ReadToEnd();
+        }
+    }
+}
--- CSharp_I_should_know/Program.cs
+++ CSharp_I_should_know/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello guy. This is a new series I've started regarding C# tips I should remember.");
+﻿using CSharp_I_should_know.EP1WhyIChooseCSharp;
+
+Console.WriteLine("Hello guy. This is a new series I've started regarding C# tips I should remember.");
+new EP1().GoToEP1();
