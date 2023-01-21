e4de751f8526acbddd4d34acbd06d468fc89eef9
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2023-01-21 19:43:25 +0700

    .

---
+++ CSharp_I_should_know/EP3DataTypes/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_I_should_know.EP3DataTypes
+{
+    internal class Class1
+    {
+    }
+}
